9cb2200df42b1b9624afadce9ce5cd2d25944cf0
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2024-02-14 11:48:06 -0600

    commit 1402202414432

--- 21_funciones.cpp
+++ 21_funciones.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#define _USE_MATH_DEFINES
 #include<cmath>
+#include<iomanip>
+
 using namespace std;
 
 //prototipo de funcion
 int elevar(int base, int potencia);
 double hipotenusa(double opuesto, double adyacente);
+bool esMultiplo(int dividendo, int divisor);
+void imprimirRectangulo(int largo, int ancho, char caracter);
+double areaCirculo(double radio);
+float  areaCirculo(float radio);
 
 int main(){
 	cout << elevar(2,8) << endl;
@@ -12,6 +19,17 @@ int main(){
 	cout << elevar(3,0) << endl;
 	cout << hipotenusa(4,6) << endl;
 	cout << hipotenusa(3,4) << endl;
+	cout << esMultiplo(5,2) << endl;
+	cout << esMultiplo(8,4) << endl;
+	imprimirRectangulo(6,8,'x');
+	//las funciones void no retornan nada, por lo tanto no
+	//devuelven un valor que se pueda usar:
+	//cout << imprimirRectangulo(6,8,'x');
+	double r1 = 2.4523324234;
+	float r2 = 4.567565456456456;
+	cout << r1 << endl;	
+	cout << setprecision(30) << areaCirculo(r1) << endl;
+	cout << setprecision(8) << areaCirculo(r2) << endl;
 	
 	return 456;
 }
@@ -39,7 +57,42 @@ double hipotenusa(double opuesto, double adyacente){
 
 /*3. Escriba una funcion que reciba dos numeros enteros y retorne true
 si el primer numero es multiplo del segundo o false en caso contrario.*/
+bool esMultiplo(int dividendo, int divisor){
+	if( dividendo%divisor == 0 )
+		return true;
+	else
+		return false;
+	//return ( dividendo%divisor == 0 ? true : false ); //usando operador ternario
+}
 
+/*4. Escriba una funcion que imprima un rectagulo de caracteres,
+dicha funcion recibira como parametros el ancho y largo del mismo
+asi como otro parametro con el caracter a usar para dibujar
+dicho rectangulo.*/
+void imprimirRectangulo(int largo, int ancho, char caracter){
+	for( int i = 0; i < largo; i++ ){
+		for(int j = 0; j < ancho; j++)
+			cout << caracter;
+		
+		cout << endl;
+	}
+}
+
+/*5. Hacer un funcion que retorne el area de un circulo,
+dicha funcion debera recibir el radio como parametro.
+Usando sobrecarga de funciones haga que la funcion pueda
+trabajar con numeros double o con numeros float.*/
+double areaCirculo(double radio){
+	return M_PI*pow(radio,2);
+}
+float  areaCirculo(float radio){
+	return M_PI*pow(radio,2);
+}
+
+/*6. Escriba una función llamada minimo que retorne
+el menor de tres números, dicha funcion debera poder
+funcionar con tres numeros enteros,
+tres numeros double o tres numeros float.*/
 
 
 
